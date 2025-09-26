70e4930cf384000f6a1eb93322531a7230920aa0
Betheony <betheony@tamu.edu>
2025-09-26 16:38:43 -0500

    test code

--- SpearmintControl.ino
+++ SpearmintControl.ino
@@ -1,1 +1,14 @@
-// TODO
+// Blink test on built-in LED (usually pin 13 on Arduino Uno)
+// Confirms the board is alive and uploading
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // ON
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);   // OFF
+  delay(1000);
+}
+
